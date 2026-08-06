a75cf598d992f9d9795eca61da187c742b1bafaa
Vishwak Batchu <vishwakbatchu45@gmail.com>
2026-08-06 18:25:57 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -39,7 +39,7 @@ app = FastAPI(title="Expense Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://expense-tracker-rip4.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
